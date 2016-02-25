c6aed788c4432a649037a61bc8c94a34b947867c
Jeff Francisco <jeff.francisco7@gmail.com>
2016-02-24 21:53:00 -0500

    Fix history page layout on mobile.

--- history.php
+++ history.php
@@ -17,7 +17,7 @@
           <h2 class="page-heading">Portuguese American Cultural Center</h2>
           
           <p>Foi fundado em 1987 pelos seguintes seis fundadores, tendo sido a primeira reunião feita no 142 Florida Park Dr. Palm Coast, FL</p>
-          <img src="images/Fundadores_poster.jpg" alt="Founders" width="670" />
+          <img src="images/Fundadores_poster.jpg" alt="Founders" class="history-img" />
         </div>
       </div>
 
