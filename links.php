6938262c2c07d0642dcfd0fae431412d126e2646
Jeff Francisco <jeff.francisco7@gmail.com>
2017-04-18 09:55:26 -0400

    Add new links

--- links.php
+++ links.php
@@ -18,10 +18,19 @@
           
           <ul>
             <li>
+              <a href="http://www.aguaconstruction.com">Agua Construction</a>
+            </li>
+            <li>
+              <a href="http://aitfl.com">AI Technologies</a>
+            </li>
+            <li>
                 <a href="http://www.alamobusinesscenter.com">Alamo Business Center</a>
             </li>
             <li>
                 <a href="http://www.amaralcustomhomes.com">Amaral Custom Homes</a>
+            </li>
+            <li>
+              <a href="http://www.arcticbreezeair.com">Arctic Breeze Air Conditioning & Heating</a>
             </li>
             <li>
               <a href="http://www.aisflagler.com">American Immigration Services</a>
@@ -36,6 +45,12 @@
               <a href="http://www.carlosdiasjr.com">Carlos Dias Jr.</a>
             </li>
             <li>
+              <a href="http://conserins.com">Conser Insurance</a>
+            </li>
+            <li>
+              <a href="http://www.craigflaglerpalms.com">Craig Flagler Palms Funeral Home & Cemetery</a>
+            </li>
+            <li>
               <a href="Resendes.pdf">FED-USA Seguros</a>
             </li>
             <li>
@@ -45,7 +60,16 @@
               <a href="http://www.idealhomespalmcoast.com">Ideal Homes of Palm Coast</a>
             </li>
             <li>
-              <a href="http://www.intracoastalbank.net">Intracoastal Bank</a>
+              <a href="https://www.intracoastalbank.net/">Intracoastal Bank</a>
+            </li>
+            <li>
+              <a href="https://www.kumon.com/palm-coast">Kumon Math and Reading Center of Palm Coast</a>
+            </li>
+            <li>
+              <a href="http://www.lusoconcerto.com">Luso Concerto</a>
+            </li>
+            <li>
+              <a href="http://www.pcchiropracticcenter.com">Palm Coast Chiropractic Center</a>
             </li>
             <li>
               <a href="http://www.palmcoastsigns.com">Palm Coast Signs and Graphics</a>
@@ -54,13 +78,25 @@
               <a href="http://www.portoexpress.com/search.htm">Porto Express</a>
             </li>
             <li>
+              <a href="http://www.portugalwinebarandgrill.com">Portugal Wine Bar & Grill</a>
+            </li>
+            <li>
               <a href="http://www.palcus.org">Portuguese-American Leadership Council of the United States</a>
             </li>
             <li>
               <a href="http://www.pratajewelers.com">Prata Jewelers</a>
             </li>
             <li>
+              <a href="http://www.showersinsurance.net">Showers Insurance</a>
+            </li>
+            <li>
               <a href="Images/SistemasDeAlarm.jpg">Sistemas De Alarm</a>
+            </li>
+            <li>
+              <a href="http://www.buyorsellinpalmcoast.com">TAG Ventures</a>
+            </li>
+            <li>
+              <a href="http://dina.buyorsellinpalmcoast.com">TAG Ventures - Dina Branquinho</a>
             </li>
             <li>
               <a href="http://www.topshelfstorage.net">Top Shelf Storage</a>
