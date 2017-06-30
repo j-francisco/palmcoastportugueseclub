b837f5a0675c7ecb1265ba9d39669cd5416d3232
Jeff Francisco <jeff.francisco7@gmail.com>
2017-06-30 15:08:34 -0400

    Add July 2017 Palmeira

--- palmeira.php
+++ palmeira.php
@@ -14,8 +14,9 @@
         </div>
 
         <?
-          $latestVersion = array("name" => "April 2017", "url" => "https://issuu.com/palmcoastportugueseclub/docs/palmeira_april_2017", "thumbnail" => "images/palmeira_thumbs/apr2017_thumb.jpg");
+          $latestVersion = array("name" => "July 2017", "url" => "https://issuu.com/palmcoastportugueseclub/docs/palmeira_july_2017", "thumbnail" => "images/palmeira_thumbs/july2017_thumb.jpg");
           $oldVersions = [
+            array("name" => "April 2017", "url" => "https://issuu.com/palmcoastportugueseclub/docs/palmeira_april_2017", "thumbnail" => "images/palmeira_thumbs/apr2017_thumb.jpg"),
             array("name" => "January 2017", "url" => "https://issuu.com/palmcoastportugueseclub/docs/palmeira_janeiro_2017", "thumbnail" => "images/palmeira_thumbs/jan2017_thumb.jpg"),
             array("name" => "October 2016", "url" => "https://issuu.com/palmcoastportugueseclub/docs/palmeira_outubro_2016", "thumbnail" => "images/palmeira_thumbs/oct2016_thumb.jpg"),
             array("name" => "July 2016", "url" => "http://issuu.com/palmcoastportugueseclub/docs/palmeira_july_2016", "thumbnail" => "images/palmeira_thumbs/july2016_thumb.jpg"),
