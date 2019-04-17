45338788de0701a652fa6fd69cdc8bae82ea553f
Jeff Francisco <jeff.francisco7@gmail.com>
2019-04-16 20:42:43 -0400

    Update links

--- links.php
+++ links.php
@@ -24,7 +24,13 @@
               <a href="http://aitfl.com">AI Technologies</a>
             </li>
             <li>
-              <a href="http://www.alamobusinesscenter.com">Alamo Business Center</a>
+              <a href="https://www.amerisbank.com">Ameris Bank</a>
+            </li>
+            <li>
+              <a href="https://aktires.com">A.K. Tires</a>
+            </li>
+            <li>
+              <a href="https://www.allaboutpricefl.com/">All About Price</a>
             </li>
             <li>
               <a href="http://volusiaflaglerac.com">All Volusia and Flagler Heating and Air</a>
@@ -55,19 +61,16 @@
               <a href="http://www.circleofhealthfp.com">Circle of Health Family Practice</a>
             </li>
             <li>
-              <a href="http://www.exceltaxwealth.com">Excel Tax & Wealth Group</a>
-            </li>
-            <li>
-              <a href="http://www.carlosdiasjr.com">Carlos Dias Jr.</a>
-            </li>
-            <li>
-              <a href="http://conserins.com">Conser Insurance</a>
-            </li>
-            <li>
               <a href="http://www.craigflaglerpalms.com">Craig Flagler Palms Funeral Home & Cemetery</a>
             </li>
             <li>
+              <a href="https://www.coldwellbankerhomes.com/fl/palm-coast/agent/dina-branquinho/aid_200591/">Dina Branquinho - Coldwell Banker</a>
+            </li>
+            <li>
               <a href="Resendes.pdf">FED-USA Seguros</a>
+            </li>
+            <li>
+              <a href="https://floridamedicalhearing.com">Florida Medical Hearing Centers</a>
             </li>
             <li>
               <a href="http://www.hackneyandassociates.com">Hackney and Associates</a>
@@ -80,6 +83,9 @@
             </li>
             <li>
               <a href="https://www.intracoastalbank.net/">Intracoastal Bank</a>
+            </li>
+            <li>
+              <a href="http://www.fortunegroupproperties.com/Joe-Barra/">Joe Barra - Fortune Group Properties</a>
             </li>
             <li>
               <a href="https://www.kumon.com/palm-coast">Kumon Math and Reading Center of Palm Coast</a>
@@ -103,22 +109,16 @@
               <a href="http://www.palcus.org">Portuguese-American Leadership Council of the United States</a>
             </li>
             <li>
-              <a href="http://www.pratajewelers.com">Prata Jewelers</a>
-            </li>
-            <li>
               <a href="http://www.propertyinpalmcoast.com">Property in Palm Coast - Steve Andrade</a>
             </li>
             <li>
               <a href="http://www.ritcheycadillac.com">Ritchey Cadillac</a>
             </li>
             <li>
+              <a href="https://www.gotconcreteproblems.net">S&D Construction and Maintenance</a>
+            </li>
+            <li>
               <a href="Images/SistemasDeAlarm.jpg">Sistemas De Alarm</a>
-            </li>
-            <li>
-              <a href="http://www.buyorsellinpalmcoast.com">TAG Ventures</a>
-            </li>
-            <li>
-              <a href="http://dina.buyorsellinpalmcoast.com">TAG Ventures - Dina Branquinho</a>
             </li>
             <li>
               <a href="https://www.mari.findpalmcoasthouses.com">Take Action Properties</a>
