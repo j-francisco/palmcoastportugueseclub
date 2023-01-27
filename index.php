3bfba9b5b8946a159c97eeb6a15cc8e42463b03f
Jeff Francisco <jeff.francisco7@gmail.com>
2023-01-26 20:02:48 -0500

    Update board of directors for 2023

--- index.php
+++ index.php
@@ -21,13 +21,10 @@
           <h4>News</h4>
           <ul class="news-list">
             <li>
-              <b><a href="files/AssembleiaDezembro2022.pdf">General Assembly - December 2022</a></b>
+              <b><a href="board.php">2021 Corpos Gerentes</a></b>
             </li>
             <li>
-              <b><a href="https://issuu.com/palmcoastportugueseclub/docs/palmeiraoutubro2022">Palmeira Quarterly Newsletter - October 2022</a></b>
-            </li>
-            <li>
-              <a href="board.php">2021 Corpos Gerentes</a>
+              <a href="https://issuu.com/palmcoastportugueseclub/docs/palmeiraoutubro2022">Palmeira Quarterly Newsletter - October 2022</a>
             </li>
             <li>
               <a href="files/PortugueseSchoolRegistrationForm.pdf">Portuguese Class Registration</a>
